147f34932e8110aa9326671dafb6538220966b1a
Michael Wilson <mwilson@maxeler.com>
2015-11-20 12:17:19 -0800

    Clean up code.

--- test/maxpower/manager/superfifo/runtime/superFifoTest.c
+++ test/maxpower/manager/superfifo/runtime/superFifoTest.c
@@ -11,6 +11,7 @@
 #define _CONCAT(x, y) x ## y
 #define CONCAT(x, y) _CONCAT(x, y)
 #define INIT_NAME CONCAT(DESIGN_NAME, _init)
+#define FREE_NAME CONCAT(DESIGN_NAME, _free)
 
 #include <stdint.h>
 #include <stdbool.h>
@@ -38,8 +39,9 @@ typedef struct PACKED {
 	uint64_t  data[4];
 } single_entry_t;
 
+#define PAGE_ALIGNMENT 4096
 #define MAX_DEPTH (128*1024*1024)
-#define STEP 512
+#define READ_LEN (1024*1024)
 
 uint8_t compare(single_entry_t* outputData, uint64_t configBase, size_t len)
 {
@@ -51,13 +53,13 @@ uint8_t compare(single_entry_t* outputData, uint64_t configBase, size_t len)
 		uint64_t expected = (configBase + entryIx);
 		if (expected != output[0]) {
 			fail = 1;
-			printf("[Entry: %zd, Quad: %zd] Mismatch: input 0x%lx, output 0x%lx\n", entryIx, 0L, expected, output[0]);
+			fprintf(stderr, "[Entry: %zd, Quad: %zd] Mismatch: input 0x%lx, output 0x%lx\n", entryIx, 0L, expected, output[0]);
 		}
 
 		for (size_t q = 1; !fail && q < quadsPerEntry; q++) {
 			if (0 != output[q]) {
 				fail = 1;
-				printf("[Entry: %zd, Quad: %zd] Mismatch: input 0x%lx, output 0x%lx\n", entryIx, q, 0L, output[q]);
+				fprintf(stderr, "[Entry: %zd, Quad: %zd] Mismatch: input 0x%lx, output 0x%lx\n", entryIx, q, 0L, output[q]);
 			}
 		}
 	}
@@ -70,11 +72,12 @@ int main(int argc, char *argv[])
 	(void) argc;
 	(void) argv;
 
-	assert((MAX_DEPTH % STEP) == 0);
+	assert((MAX_DEPTH % READ_LEN) == 0);
 
+	// init
 	max_file_t *maxfile = INIT_NAME();
 	if(!maxfile) {
-		printf("Failed to init MAX file\n");
+		fprintf(stderr, "Failed to init MAX file\n");
 		return -1;
 	}
 
@@ -85,7 +88,7 @@ int main(int argc, char *argv[])
 
 	max_engine_t *engine = max_load(maxfile, device_name);
 	if(!engine) {
-		printf("Failed to open Max device\n");
+		fprintf(stderr, "Failed to open Max device\n");
 		exit(-1);
 	}
 
@@ -97,11 +100,14 @@ int main(int argc, char *argv[])
 	max_actions_t *action = max_actions_init(maxfile, NULL);
 	max_run(engine, action);
 	max_actions_free(action);
+	action = NULL;
 
-
+	// configure
 	void *configWordBuffer = NULL;
-	posix_memalign(&configWordBuffer, 4096, STEP * sizeof(configWord_t));
-	max_llstream_t *configWordStream = max_llstream_setup(engine, "configWord", STEP, sizeof(configWord_t), configWordBuffer);
+	posix_memalign(&configWordBuffer, PAGE_ALIGNMENT, sizeof(configWord_t));
+	assert(configWordBuffer != NULL);
+
+	max_llstream_t *configWordStream = max_llstream_setup(engine, "configWord", 1, sizeof(configWord_t), configWordBuffer);
 
 	uint64_t configBase = 0;
 	printf("Sending config word...\n");
@@ -112,31 +118,52 @@ int main(int argc, char *argv[])
 	configWord->base = configBase;
 	max_llstream_write(configWordStream, 1);
 
+	// prompt user
+	printf("Press ENTER to begin test...\n");
+	getchar();
+
+	// test
+	printf("Streaming 'read_fifo'...\n");
+
+	single_entry_t *outputData = calloc(READ_LEN, sizeof(single_entry_t));
+	assert(outputData != NULL);
+
+	action = max_actions_init(maxfile, NULL);
+	max_queue_output(action, "read_fifo", outputData, sizeof(single_entry_t) * READ_LEN);
+	max_disable_reset(action);
+	max_disable_validation(action);
+	max_enable_partial_memory(action);
+
 	uint8_t fail = 0;
-	for(size_t i = 0; i < MAX_DEPTH / STEP; i++ ){
-
-		single_entry_t *outputData = calloc(STEP, sizeof(single_entry_t));
-		assert(outputData != NULL);
-
-		printf("Streaming 'read_fifo'...\n"); fflush(stdout);
-		action = max_actions_init(maxfile, NULL);
-		max_queue_output(action, "read_fifo", outputData, sizeof(single_entry_t) * STEP);
-		max_disable_reset(action);
-		max_disable_validation(action);
-		max_enable_partial_memory(action);
+	for(size_t i = 0; i < (MAX_DEPTH / READ_LEN); i++ ){
 		max_run(engine, action);
-		max_actions_free(action);
 
-		printf("Comparing...\n"); fflush(stdout);
-		size_t start = i * STEP;
+		size_t start = i * READ_LEN;
 		size_t base = configBase + start;
-		fail |= compare(outputData, base, STEP);
-
-		free(outputData);
-		outputData = NULL;
+		fail |= compare(outputData, base, READ_LEN);
 	}
-	
 
+	// clean up
+	max_llstream_release(configWordStream);
+	configWordStream = NULL;
+
+	free(configWordBuffer);
+	configWordBuffer = NULL;
+
+	max_actions_free(action);
+	action = NULL;
+
+	free(outputData);
+	outputData = NULL;
+
+	max_unload(engine);
+	engine = NULL;
+
+	FREE_NAME();
+	maxfile = NULL;
+
+	// report result
 	printf("%s\n", fail ? "FAILED!" : "Success");
+
 	return fail;
 }
