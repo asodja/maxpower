7276e9d6924edad0fc9c606c667124de2b36053c
Michael Wilson <mwilson@maxeler.com>
2015-11-19 16:34:11 -0800

    Split checking into chunks to reduce memory requirements.

--- test/maxpower/manager/superfifo/runtime/superFifoTest.c
+++ test/maxpower/manager/superfifo/runtime/superFifoTest.c
@@ -39,11 +39,39 @@ typedef struct PACKED {
 } single_entry_t;
 
 #define MAX_DEPTH (128*1024*1024)
+#define STEP 512
+
+uint8_t compare(single_entry_t* outputData, uint64_t configBase, size_t len)
+{
+	uint8_t fail = 0;
+	for (size_t entryIx=0; entryIx < len; entryIx++) {
+		uint64_t *output = (uint64_t *)outputData[entryIx].data;
+		size_t quadsPerEntry = sizeof(single_entry_t) / sizeof(uint64_t);
+
+		uint64_t expected = (configBase + entryIx);
+		if (expected != output[0]) {
+			fail = 1;
+			printf("[Entry: %zd, Quad: %zd] Mismatch: input 0x%lx, output 0x%lx\n", entryIx, 0L, expected, output[0]);
+		}
+
+		for (size_t q = 1; !fail && q < quadsPerEntry; q++) {
+			if (0 != output[q]) {
+				fail = 1;
+				printf("[Entry: %zd, Quad: %zd] Mismatch: input 0x%lx, output 0x%lx\n", entryIx, q, 0L, output[q]);
+			}
+		}
+	}
+
+	return fail;
+}
 
 int main(int argc, char *argv[])
 {
 	(void) argc;
 	(void) argv;
+
+	assert((MAX_DEPTH % STEP) == 0);
+
 	max_file_t *maxfile = INIT_NAME();
 	if(!maxfile) {
 		printf("Failed to init MAX file\n");
@@ -71,53 +99,43 @@ int main(int argc, char *argv[])
 	max_actions_free(action);
 
 
-	srand(time(NULL));
-	single_entry_t *outputData = calloc(MAX_DEPTH, sizeof(single_entry_t));
-
 	void *configWordBuffer = NULL;
-	posix_memalign(&configWordBuffer, 4096, 512 * sizeof(configWord_t));
-	max_llstream_t *configWordStream = max_llstream_setup(engine, "configWord", 512, sizeof(configWord_t), configWordBuffer);
+	posix_memalign(&configWordBuffer, 4096, STEP * sizeof(configWord_t));
+	max_llstream_t *configWordStream = max_llstream_setup(engine, "configWord", STEP, sizeof(configWord_t), configWordBuffer);
 
 	uint64_t configBase = 0;
 	printf("Sending config word...\n");
 	void *configWordSlot;
-	while (max_llstream_write_acquire(configWordStream, 1, &configWordSlot) != 1) usleep(10);
+	while (max_llstream_write_acquire(configWordStream, 1, &configWordSlot) != 1);
 	configWord_t *configWord = configWordSlot;
 	configWord->wordCount = MAX_DEPTH;
 	configWord->base = configBase;
 	max_llstream_write(configWordStream, 1);
 
-	getchar();
-
-
-
-	printf("Streaming 'read_fifo'...\n"); fflush(stdout);
-	action = max_actions_init(maxfile, NULL);
-	max_queue_output(action, "read_fifo", outputData, sizeof(single_entry_t) * MAX_DEPTH);
-	max_disable_reset(action);
-	max_disable_validation(action);
-	max_enable_partial_memory(action);
-	max_run(engine, action);
-	max_actions_free(action);
-
-	printf("Comparing...\n"); fflush(stdout);
 	uint8_t fail = 0;
-	for (size_t entryIx=0; entryIx < MAX_DEPTH; entryIx++) {
-		uint64_t *output = (uint64_t *)outputData[entryIx].data;
-		size_t quadsPerEntry = sizeof(single_entry_t) / sizeof(uint64_t);
+	for(size_t i = 0; i < MAX_DEPTH / STEP; i++ ){
 
-		uint64_t expected = (configBase + entryIx);
-		if (expected != output[0]) {
-			fail = 1;
-			printf("[Entry: %zd, Quad: %zd] Mismatch: input 0x%lx, output 0x%lx\n", entryIx, 0L, expected, output[0]);
-		}
-		for (size_t q = 1; !fail && q < quadsPerEntry; q++) {
-			if (0 != output[q]) {
-				fail = 1;
-				printf("[Entry: %zd, Quad: %zd] Mismatch: input 0x%lx, output 0x%lx\n", entryIx, q, 0L, output[q]);
-			}
-		}
+		single_entry_t *outputData = calloc(STEP, sizeof(single_entry_t));
+		assert(outputData != NULL);
+
+		printf("Streaming 'read_fifo'...\n"); fflush(stdout);
+		action = max_actions_init(maxfile, NULL);
+		max_queue_output(action, "read_fifo", outputData, sizeof(single_entry_t) * STEP);
+		max_disable_reset(action);
+		max_disable_validation(action);
+		max_enable_partial_memory(action);
+		max_run(engine, action);
+		max_actions_free(action);
+
+		printf("Comparing...\n"); fflush(stdout);
+		size_t start = i * STEP;
+		size_t base = configBase + start;
+		fail |= compare(outputData, base, STEP);
+
+		free(outputData);
+		outputData = NULL;
 	}
+	
 
 	printf("%s\n", fail ? "FAILED!" : "Success");
 	return fail;
